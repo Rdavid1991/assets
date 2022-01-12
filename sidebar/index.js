84ef661d8220d3c21184fc2970e9947ab76a64ab
Rdavid1991 <42180920+Rdavid1991@users.noreply.github.com>
2022-01-11 22:52:09 -0500

    iconos

--- sidebar/index.js
+++ sidebar/index.js
@@ -3,7 +3,8 @@ const toggleOpen = document.querySelector("#menu-button")
 toggleOpen.addEventListener("click", (e) => {
     toggleOpen.classList.toggle("change");
     document.querySelector(".container__sidebar").classList.toggle("open")
-    document.querySelectorAll("div>a").forEach((element) => {
+    document.querySelectorAll(".option__content>span").forEach((element) => {
         element.classList.toggle("open")
+        
     })
 })
